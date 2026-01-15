8750d29e498dfc2381cbefa97feed5df37957d0d
Shyam <s7hyamweb@gmail.com>
2026-01-15 20:51:49 +0530

    connection to mongodb atlas

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export default async function connectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+
+    console.log(
+      `\n MongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`,
+    );
+  } catch (error) {
+    console.log("Mongo DB connection Failed", error);
+    process.exit(1);
+  }
+}
